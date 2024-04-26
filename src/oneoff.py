3d7fc5c12de094a24b3da70c1c9afa9d238a184f
David Shaw <dshaw11@utk.edu>
2024-04-26 15:39:08 -0400

    not sure if this is oging to work

--- src/oneoff.py
+++ src/oneoff.py
@@ -3,25 +3,30 @@ from PIL import ImageTk, Image
 import math
 
 
-ramp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-ramp_length = len(ramp)
+# ramp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+def main():
+    ramp = "@%#*+=-:. " 
+    ramp_length = len(ramp)
 
-print(ramp_length)
+    print(ramp_length)
+    output = []
+    with Image.open('./test.jpg') as image:
+        px = image.load()
+        width, height = image.size
+    # image = image.convert('L')
+    # print(image.format, image.size, image.mode)
 
-with Image.open('./test.jpg') as image:
-    px = image.load()
-    width, height = image.size
-# image = image.convert('L')
-# print(image.format, image.size, image.mode)
+    # image.show():
+    for x in range(width):
+        for y in range(height):
+            r, g, b = px[x,y]
+            gray_val = 0.3 * r + 0.59 * g + 0.11 * b
+            gray_val_int = int(gray_val)
+            px[x, y] = (int(gray_val), int(gray_val), int(gray_val))
+            char = ramp[math.ceil(((ramp_length - 1) * gray_val) / 255)]
+            output[x,y] = char
+            
+    return output
 
-# image.show():
-for x in range(width):
-    for y in range(height):
-        r, g, b = px[x,y]
-        gray_val = 0.3 * r + 0.59 * g + 0.11 * b
-        gray_val_int = int(gray_val)
-        px[x, y] = (int(gray_val), int(gray_val), int(gray_val))
-        
-        
-
-image.show()
+    # image.show()
+main()
