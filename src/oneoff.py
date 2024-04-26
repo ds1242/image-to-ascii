f4056b79501b5e3c47f287456de549e8addc1c95
David Shaw <dshaw11@utk.edu>
2024-04-26 14:54:07 -0400

    more work on converting to ascii

--- src/oneoff.py
+++ src/oneoff.py
@@ -1,8 +1,13 @@
 from tkinter import Tk, BOTH, Canvas
 from PIL import ImageTk, Image
+import math
 
 
-# ramp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.'
+ramp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+ramp_length = len(ramp)
+
+print(ramp_length)
+
 with Image.open('./test.jpg') as image:
     px = image.load()
     width, height = image.size
@@ -13,7 +18,10 @@ with Image.open('./test.jpg') as image:
 for x in range(width):
     for y in range(height):
         r, g, b = px[x,y]
-        gray_val = int(0.3 * r + 0.59 * g + 0.11 * b)
-        px[x, y] = (gray_val, gray_val, gray_val)
+        gray_val = 0.3 * r + 0.59 * g + 0.11 * b
+        gray_val_int = int(gray_val)
+        px[x, y] = (int(gray_val), int(gray_val), int(gray_val))
+        
+        
 
 image.show()
